41e2d5fbefb4f32a288e9a679c7bd09cdf0d55cf
Motiullah Sajit <sajitmotiullah@gamil.com>
2021-05-19 10:00:16 +0600

    change image height

--- src/components/Main/Blog/Blog.js
+++ src/components/Main/Blog/Blog.js
@@ -1,19 +1,19 @@
 import React from 'react';
 
 const Blog = ({ blog }) => {
-    const { title, shortDisprition, imgUrl, blogLink } = blog;
-    return (
-        <article data-aos="fade-up" className='col-md-4'>
-            <div className="card h-100">
-                <img src={imgUrl} className="card-img-top h-75" alt="..." />
-                <div className="card-body">
-                    <h5 className="card-title">{title}</h5>
-                    <p className="card-text">{shortDisprition}</p>
-                    <a href={blogLink} className='color-brand mt-3 text-decoration-none'><strong>Read More</strong></a>
-                </div>
-            </div>
-        </article>
-    );
+  const { title, shortDisprition, imgUrl, blogLink } = blog;
+  return (
+    <article data-aos="fade-up" className='col-md-4'>
+      <div className="card h-100">
+        <img src={imgUrl} className="card-img-top h-50" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{shortDisprition}</p>
+          <a href={blogLink} className='color-brand mt-3 text-decoration-none'><strong>Read More</strong></a>
+        </div>
+      </div>
+    </article>
+  );
 };
 
 export default Blog;
