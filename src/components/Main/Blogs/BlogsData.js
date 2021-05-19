44eb3bcac2fe69af85a7f533c432fc2ee50e81a3
Motiullah Sajit <sajitmotiullah@gamil.com>
2021-05-19 10:00:52 +0600

    change all the blogs data

--- src/components/Main/Blogs/BlogsData.js
+++ src/components/Main/Blogs/BlogsData.js
@@ -1,23 +1,23 @@
 export const Blogsdata = [
-    {
-        id: 1,
-        title: 'Staring Journey',
-        shortDisprition: 'As a beginner like everyone, I also started with HTML then CSS, then a framework of CSS which is called Bootstrap, and then the basic JavaScript.',
-        imgUrl: 'https://i.ibb.co/2M1krDc/1-ZPm-QG3-Am-Ca-Sb-W5-Ijh-Pts-Q.jpg',
-        blogLink: 'https://motiullahsajit.medium.com/my-one-and-a-half-month-journey-of-learning-web-dev-e9f6b0e88fba'
-    },
-    {
-        id: 2,
-        title: 'How React works',
-        shortDisprition: 'React.js is generally thought of as the view layer in an application. You might have used libraries like Handlebars or jQuery in the past.',
-        imgUrl: 'https://i.ibb.co/HxhSqvS/image005.jpg',
-        blogLink: 'https://motiullahsajit.medium.com/react-js-is-just-the-view-thats-it-91ecee68315e'
-    },
-    {
-        id: 3,
-        title: '5 most important topics in Js',
-        shortDisprition: 'The concepts that we will explore Destructuring, Spread Syntax, Rest Syntax, Array methods,Value vs. Reference Variable Assignment',
-        imgUrl: 'https://i.ibb.co/HrmdrdN/1-6ahb-Wjp-g9hqha-TDSJOL1-Q.png',
-        blogLink: 'https://motiullahsajit.medium.com/5-most-important-topics-in-js-e6f7636da37'
-    }
+  {
+    id: 1,
+    title: 'Random Topics In JavaScript',
+    shortDisprition: 'JavaScript is a scripting or programming language. JavaScript is used to add interactions in the websites, it is called the heart of a website.',
+    imgUrl: 'https://miro.medium.com/max/3600/1*6ahbWjp_g9hqhaTDSJOL1Q.png',
+    blogLink: 'https://motiullahsajit.medium.com/random-topics-in-javascript-8c5baa4167b9'
+  },
+  {
+    id: 2,
+    title: 'React Jetpack',
+    shortDisprition: "React is not exactly a framework it's a library.React is a JavaScript library for building user interfaces.",
+    imgUrl: 'https://miro.medium.com/max/2560/1*zvg6SggKvPRpNHUpDuzyrQ.jpeg',
+    blogLink: 'https://motiullahsajit.medium.com/react-jetpack-1751d6c56379'
+  },
+  {
+    id: 3,
+    title: 'Frequently asked questions JavaScript interview',
+    shortDisprition: 'In JavaScript, a truthy value is a value that is considered true when encountered in a Boolean context.',
+    imgUrl: 'https://miro.medium.com/max/1600/0*K-bHYySR3eurOY4O.png',
+    blogLink: 'https://motiullahsajit.medium.com/frequently-asked-questions-javascript-interview-c48f6ea77b32'
+  }
 ]
